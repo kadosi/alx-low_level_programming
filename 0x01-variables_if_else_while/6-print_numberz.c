7217921b3ace6854a4738772868658bd4e369d32
kadosi <paulmwas2470@gmail.com>
2023-07-17 06:07:04 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - intilization entry
+ * Description: base 10 starting from 0
+ * Return: 0 succession
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return  (0);
+}
+
