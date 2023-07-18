f65a0cd544e05e586bf59dd773286b3ae3bc9fa6
kadosi <paulmwas2470@gmail.com>
2023-07-18 00:44:38 -0700

    alpha i bet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - Make the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
