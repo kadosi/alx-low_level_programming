fe067ae491a9a063dc9744ebaf5e6e2e7e98f087
kadosi <paulmwas2470@gmail.com>
2023-07-19 04:44:30 -0700

    abs haha

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - functionthat computes the absolute value
+ * @x: integer
+ * Return: 0 succession
+ */
+int _abs(int x)
+{
+
+	if (x >= 0)
+	{
+		return (x);
+	}
+	return (x * -1);
+}
