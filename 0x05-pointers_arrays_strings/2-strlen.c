1190662ab312ac9d13e7dcae76a2b2187300f016
kadosi <paulmwas2470@gmail.com>
2023-07-25 00:41:16 -0700

    haha lengthy

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string
+ * Return: length of a string.
+ */
+
+int _strlen(char *s)
+{
+	int x = 0;
+	int len = 0;
+
+	for (; x[s] != '\0'; x++)
+	{
+		len++;
+	}
+	return (len);
+}
+
