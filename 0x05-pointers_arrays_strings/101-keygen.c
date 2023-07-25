0ea3defe91dc71b70985f53c79ba314b228e6c54
kadosi <paulmwas2470@gmail.com>
2023-07-25 02:12:54 -0700

    crack the password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+char generate_random_char()
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	
+	int index = rand() % (sizeof(charset) - 1);
+	return charset[index];
+
+}
+
+int main()
+{
+    srand(time(NULL));
+
+    char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+    int i;
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = generate_random_char();
+    }
+    password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string
+
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
