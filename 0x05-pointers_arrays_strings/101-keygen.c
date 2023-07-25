079225f2be2589e4e27c2a51c7e084541f6d3e91
kadosi <paulmwas2470@gmail.com>
2023-07-25 02:30:43 -0700

    password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define PASSWORD_LENGTH 15
 
+char generate_random_char()
+{
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    int index = rand() % (sizeof(charset) - 1);
+    return charset[index];
+}
+
 int main()
 {
-    char password[PASSWORD_LENGTH + 1] = "Tada! Congrats"; /* +1 for the null terminator */
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    srand(time(NULL));
+
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = generate_random_char();
+    }
+    password[PASSWORD_LENGTH] = '\0';
 
     printf("%s\n", password);
 
