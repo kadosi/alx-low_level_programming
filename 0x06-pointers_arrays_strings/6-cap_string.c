a4cb08843df8859265b9cd9ca3e3af278b1ba876
kadosi <paulmwas2470@gmail.com>
2023-07-27 01:50:07 -0700

    aint capping its in caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to be modified.
+ *
+ * Return: The modified string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (islower(s[i]))
+		{
+			if (s[i - 1] == ' ' || s[i - 1] == '\n'
+		|| s[i - 1] == '\t' || s[i - 1] == ','
+		|| s[i - 1] == ';' || s[i - 1] == '!'
+		|| s[i - 1] == '?' || s[i - 1] == '"'
+		|| s[i - 1] == '(' || s[i - 1] == ')'
+		|| s[i - 1] == '{' || s[i - 1] == '}'
+		|| s[i - 1] == '.')
+			{
+				s[i] = toupper(s[i]);
+			}
+		}
+		i++;
+	}
+	return (s);
+}
