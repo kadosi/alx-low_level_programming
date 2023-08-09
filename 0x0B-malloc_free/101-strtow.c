33b4d2a57ce2490e6f5697e46fe8f227da5bd12a
kadosi <paulmwas2470@gmail.com>
2023-08-09 00:55:03 -0700

    added valgrid to free space

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 
+int count_words(char *str);
+void free_words(char **words);
 /**
  * strtow - Splits a string into words.
  * @str: The string to be split.
@@ -40,3 +42,43 @@ char **strtow(char *str)
 	return (words);
 }
 
+/**
+ * free_words - Frees memory allocated for an array of words.
+ * @words: The array of words to be freed.
+ */
+void free_words(char **words)
+{
+	if (words != NULL)
+	{
+		int i;
+
+		for (i = 0; words[i] != NULL; i++)
+			free(words[i]);
+		free(words);
+	}
+}
+
+/**
+ * count_words - Counts the number of words in a string.
+ * @str: The string to be counted.
+ *
+ * Return: The number of words in the string.
+ */
+int count_words(char *str)
+{
+	int count = 0;
+	int is_word = 0;
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n')
+			is_word = 0;
+		else if (is_word == 0)
+		{
+			is_word = 1;
+			count++;
+		}
+	}
+	return (count);
+}
