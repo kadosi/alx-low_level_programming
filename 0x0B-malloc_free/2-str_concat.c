6a09023a056710ea840ae46171f4282681b1b423
kadosi <paulmwas2470@gmail.com>
2023-08-08 23:36:03 -0700

    concat string

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	size_t len_s1;
+	size_t len_s2;
+	char *result;
+
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	len_s1 = strlen(s1);
+	len_s2 = strlen(s2);
+
+	result = (char *)malloc(len_s1 + len_s2 + 1);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(result, s1);
+	strcat(result, s2);
+
+	return (result);
+}
