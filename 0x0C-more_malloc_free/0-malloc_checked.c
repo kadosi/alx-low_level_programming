2cea4da56ba5df123e4426b7aa5123f1ba37c3b7
kadosi <paulmwas2470@gmail.com>
2023-08-10 23:34:13 -0700

    push and create

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return(p);
+}
