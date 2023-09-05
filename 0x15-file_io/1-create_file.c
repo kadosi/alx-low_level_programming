4c9cd9a7fd29a23f853d1954ed45227e03160b65
kadosi <Paulmwas247@gmail.com>
2023-09-05 03:22:07 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,26 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t write_count;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-	if (fd == -1)
-		return (-1);
-
 	if (text_content != NULL)
 	{
-		write_count = write(fd, text_content, strlen(text_content));
-
-		if (write_count == -1)
-		{
-			close(fd);
-			return (-1);
-		}
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	close(fd);
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
 	return (1);
 }
